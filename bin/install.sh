523a4d5af894f44cbe9dfc7bbaf7d64bba5d1ac9
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-01-04 09:40:39 +0800

    update

---
+++ bin/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pip install virtualenv -i https://pypi.tuna.tsinghua.edu.cn/simple/
+virtualenv clip
+source clip/bin/activate
+
+pip install torch -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip install git+https://gitee.com/xdjiangkai/CLIP
+pip install clip-score -i https://pypi.tuna.tsinghua.edu.cn/simple/
